29cc8b056be8d6afdf612e80d21a86920cbe32d3
Demon000 <demonsingur@gmail.com>
2019-06-23 23:16:58 +0200

    Revert "perseus: Build custom LiveDisplay HAL"
    This reverts commit 4990a59349da97782ad55336f5deb562b7bbd464.

--- device.mk
+++ device.mk
@@ -37,7 +37,3 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:system/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/keylayout/uinput-goodix.kl:system/usr/keylayout/uinput-goodix.kl
-
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    lineage.livedisplay@2.0-service.xiaomi_sdm845

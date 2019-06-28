91916de2b653b8ed854f92f7af836b00c5d2cdc9
Demon000 <demonsingur@gmail.com>
2019-06-28 07:42:10 +0200

    Revert "perseus: Import stock NFC configs"
    This reverts commit a9beecc8027ffd5fa0ab0b061397349d3a279c89.

--- device.mk
+++ device.mk
@@ -59,7 +59,3 @@ PRODUCT_PACKAGES += \
     NQNfcNci \
     SecureElement \
     Tag
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/nfc/libnfc-brcm.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-brcm.conf \
-    $(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf

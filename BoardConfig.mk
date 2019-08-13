963ec7de6c7ef193bd8f05a2435f4137229c721f
Demon000 <demonsingur@gmail.com>
2019-08-13 20:26:29 +0200

    cepheus: move system root image config to common tree
    Change-Id: I11ac5926e93a4b4042eaeab80c68bba4e421e658

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,9 +21,6 @@ DEVICE_FRAMEWORK_MANIFEST_FILE += $(DEVICE_PATH)/framework_manifest.xml
 # Kernel
 TARGET_KERNEL_CONFIG := cepheus_defconfig
 
-# Partitions
-BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
-
 # Inherit from the proprietary version
 -include vendor/xiaomi/cepheus/BoardConfigVendor.mk
 

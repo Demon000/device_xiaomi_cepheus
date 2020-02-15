49161416ea41a4e2d282750e9c4f11d202968f21
Demon000 <demonsingur@gmail.com>
2020-02-15 09:48:01 +0000

    cepheus: remove displayfeature props
    Change-Id: I3255a5087035bae38bdd197bd68c498f0345e003

--- device-props.mk
+++ device-props.mk
@@ -13,15 +13,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=440
 
-# Display features
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.displayfeature.histogram.enable=true \
-    ro.eyecare.brightness.threshold=11 \
-    ro.eyecare.brightness.level=5 \
-    ro.hist.brightness.threshold=7 \
-    sys.displayfeature.hbm.enable=true \
-    sys.displayfeature_hidl=true
-
 # Product
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.product.mod_device=cepheus_eea_global
